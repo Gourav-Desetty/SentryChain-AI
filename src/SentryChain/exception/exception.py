0554c5c18da119b3a931b40d2a64ba7d5b3355f4
Gourav-Desetty <139993532+Gourav-Desetty@users.noreply.github.com>
2026-03-06 22:19:04 +0530

    Fixed error in exception.py

--- src/SentryChain/exception/exception.py
+++ src/SentryChain/exception/exception.py
@@ -1,16 +1,17 @@
 import os, sys
 
 class CustomException(Exception):
-    def __init__(self, error_message, error_detail: Exception):
+    def __init__(self, error_message, error_detail):
         self.error_message = error_message
-        _, _, exc_tb = sys.exc_info()
+        self.error_detail = error_detail
+        _, _, exc_tb = error_detail.exc_info()
+
+        self.line_no = None
+        self.file_name = None
 
         if exc_tb is not None:
             self.line_no = exc_tb.tb_lineno
             self.file_name = exc_tb.tb_frame.f_code.co_filename
-
-        self.line_no = None
-        self.file_name = None
 
     def __str__(self):
         return f"Error occured in python script {self.file_name}, line number {self.line_no}, error message {str(self.error_message)}"
